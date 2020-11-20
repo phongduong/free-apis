39644166acb8b7c614aac91cf912ff63d40f7dc0
Phong Duong <phongduong1710@gmail.com>
2020-11-20 14:37:22 +0700

    Update nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,11 +7,15 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'free-apis',
+    title: 'Free APIs',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'A list of free APIs',
+      },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
     script: [
